947a15bc0aada17d50615f7e20b463a7492ce5ba
oshrat <oshrat.y20@gmail.com>
2017-09-24 22:13:06 +0300

    added comments

--- bookstore-cli/bookstore-cli/TestBookstoreCli.py
+++ bookstore-cli/bookstore-cli/TestBookstoreCli.py
@@ -4,6 +4,7 @@ from BookstoreCLI import BookstoreCLI
 
 class TestBookstoreCli(unittest.TestCase):
 
+ #The test checks if a specific user exists or not.
     def test_getUser_not_found(self):  
         user = BookstoreCLI.do_getUser(self, 'banana')           
         self.assertEqual(user, 'not found')
@@ -12,6 +13,7 @@ class TestBookstoreCli(unittest.TestCase):
         user = BookstoreCLI.do_getUser(self, 'sparta')           
         self.assertEqual(user.email, 'sparta@bookstore.com')
 
+#The test check if a user that we try to create allready exists or not
     def test_createUser_already_exist(self):
         user = BookstoreCLI.do_createUser(self, 'sparta password')
         self.assertEqual(user, 'user already exist')
